7fbd439ce89e0c5d67a449981669bfd4d92af88d
aquadzn <williamjcqs8@gmail.com>
2019-10-04 16:42:33 +0200

    (acp :robot:)

--- datasets/get_news.sh
+++ datasets/get_news.sh
@@ -4,8 +4,7 @@ wget https://www64.zippyshare.com/d/SA2Zy4VH/1279/articles.txt \
     --referer='https://www64.zippyshare.com/v/SA2Zy4VH/file.html' \
     --cookies=off --header "Cookie: JSESSIONID=10AB0BCA43F4DD6A24B98D50BF8A1B47" \
     --user-agent='Mozilla/5.0 (Windows NT 6.0) Gecko/20100101 Firefox/14.0.1'
+# wget --referer=https://anonfiles.com/ https://cdn-15.anonfile.com/q4P6C87bn4/3247fafd-1570200652/articles.txt
 
-# Lien de secours:
-#   - http://dl.free.fr/getfile.pl?file=/zoPhi27J
-#   -
+# Lien de secours: - http://dl.free.fr/getfile.pl?file=/zoPhi27J
 
