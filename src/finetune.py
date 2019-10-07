4f8be32d3507f59424285cd5a9896b7529cbb154
William Jacques <46140458+aquadzn@users.noreply.github.com>
2019-10-07 16:51:07 +0200

    Create finetune.py

---
+++ src/finetune.py
@@ -0,0 +1,24 @@
+import gpt_2_simple as gpt2
+from datetime import datetime
+import time
+
+
+gpt2.download_gpt2(model_name="124M")
+file_name = "./articles.txt"
+
+sess = gpt2.start_tf_sess()
+gpt2.finetune(sess,
+              dataset=file_name,
+              model_name='124M',
+              steps=1000,
+              restore_from='fresh',
+              run_name='run1',
+              print_every=100,
+              learning_rate=1e-4,
+              sample_every=500,
+              save_every=1000)
+              
+gpt2.generate(sess, run_name='run1')
+
+# sess = gpt2.start_tf_sess()
+# gpt2.load_gpt2(sess, run_name='run1')
