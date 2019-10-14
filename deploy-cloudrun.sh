c19b53ddeabb6a1c5c168fd1dd1039442750c2ca
aquadzn <williamjcqs8@gmail.com>
2019-10-14 15:57:49 +0200

    Add folders

--- deploy-cloudrun.sh
+++ deploy-cloudrun.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-
+for d in gpt2-models/*; do
+    [ -f "$d" ] && continue 
+    tag=$(echo "$d" | cut -d "/" -f2-)
+    gcloud builds submit "$d" --tag "gcr.io/deep-learning-254808/$tag"
+done
