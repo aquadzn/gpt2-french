f11e1ec13435559e59ca267fad3a86b6a7d39c1c
aquadzn <williamjcqs8@gmail.com>
2019-10-14 17:33:44 +0200

    Add script to automatize deployment

--- deploy-cloudrun.sh
+++ deploy-cloudrun.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+echo -e "Warning!\nThis can take some time because of the size of images.\n"
 for d in gpt2-models/*; do
+    # Push images to GCR
     [ -f "$d" ] && continue 
     tag=$(echo "$d" | cut -d "/" -f2-)
     gcloud builds submit "$d" --tag "gcr.io/deep-learning-254808/$tag"
+    
+    # Create CloudRun services
+    gcloud beta run deploy "$tag" \
+        --image "gcr.io/deep-learning-254808/$tag" \
+        --platform=managed \
+        --allow-unauthenticated \
+        --region=europe-west1 \
+        --concurrency=1 \
+        --memory=2Gi
+
+#gcloud beta run services list --platform=managed | grep -i -o -E '[a-z]+\@[a-z]+.com'
+
 done
+echo "Done!"
