38f519ef61ddf55ccec77049d91c4bed62052d8e
aquadzn <williamjcqs8@gmail.com>
2019-10-15 14:48:43 +0200

    Modified deploy_cloudrun.sh

--- deploy_cloudrun.sh
+++ deploy_cloudrun.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
+read -p 'Do yo want to delete images on GCR after building CloudRun services ? (y/n) : ' question
 echo -e "Warning!\nThis can take some time because of the size of images.\n"
-for d in gpt2-models/*; do
-    # Push images to GCR
-    [ -f "$d" ] && continue 
-    tag=$(echo "$d" | cut -d "/" -f2-)
-    gcloud builds submit "$d" --tag "gcr.io/deep-learning-254808/$tag"
-    
-    # Create CloudRun services
-    gcloud beta run deploy "$tag" \
-        --image "gcr.io/deep-learning-254808/$tag" \
-        --platform=managed \
-        --allow-unauthenticated \
-        --region=us-east1 \
-        --concurrency=1 \
-        --memory=2Gi
-done
-echo -e "\nDone!\nURLs of Cloud Run services:\n\n$(gcloud beta run services list --platform=managed | grep -Po 'https.?://\S+')"
+
+if [ "$question" != "${question#[Yy]}" ]; then
+    for d in gpt2-models/*; do
+        # Push images to GCR
+        [ -f "$d" ] && continue 
+        tag=$(echo "$d" | cut -d "/" -f2-)
+        gcloud builds submit "$d" --tag "gcr.io/deep-learning-254808/$tag"
+
+        # Create CloudRun services
+        gcloud beta run deploy "$tag" \
+            --image "gcr.io/deep-learning-254808/$tag" \
+            --platform=managed \
+            --allow-unauthenticated \
+            --region=us-east1 \
+            --concurrency=1 \
+            --memory=2Gi
+
+        # Delete images on GCR to save money?
+        gcloud container images delete "gcr.io/deep-learning-254808/$tag"
+    done
+else
+    for d in gpt2-models/*; do
+        # Push images to GCR
+        [ -f "$d" ] && continue 
+        tag=$(echo "$d" | cut -d "/" -f2-)
+        gcloud builds submit "$d" --tag "gcr.io/deep-learning-254808/$tag"
+
+        # Create CloudRun services
+        gcloud beta run deploy "$tag" \
+            --image "gcr.io/deep-learning-254808/$tag" \
+            --platform=managed \
+            --allow-unauthenticated \
+            --region=us-east1 \
+            --concurrency=1 \
+            --memory=2Gi
+    done
+fi
+
+echo -e "\nDone!\nURLs of Cloud Run services:\n\n$(gcloud beta run services list --platform=managed |grep -Po 'https.?://\S+')"
