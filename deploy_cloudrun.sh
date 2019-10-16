31bbfb480ae2e4a14a971ce1c82d4fc84e9b9008
aquadzn <williamjcqs8@gmail.com>
2019-10-16 17:43:02 +0200

    Change script

--- deploy_cloudrun.sh
+++ deploy_cloudrun.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
+read -p 'Please enter your project idea (ex: my-project-204828) : ' project_id
+echo -e '\n'
+read -p 'Please enter the tag you want for your image (ex: gpt2) : ' tag
+echo -e "\nWarning!\nThis can take some time because of the size of images.\n"
+
+gcloud builds submit "./gpt2-model" --tag "gcr.io/$project_id/$tag"
+
+gcloud beta run deploy "$tag" \
+    --image "gcr.io/$project_id/$tag" \
+    --platform=managed \
+    --allow-unauthenticated \
+    --region=us-east1 \
+    --concurrency=1 \
+    --memory=2Gi
+
 read -p 'Do yo want to delete images on GCR after building CloudRun services ? (y/n) : ' question
-echo -e "Warning!\nThis can take some time because of the size of images.\n"
-
-for d in gpt2-models/*; do
-
-    # Push images to GCR
-    [ -f "$d" ] && continue 
-    tag=$(echo "$d" | cut -d "/" -f2-)
-    gcloud builds submit "$d" --tag "gcr.io/deep-learning-254808/$tag"
-
-    # Create CloudRun services
-    gcloud beta run deploy "$tag" \
-        --image "gcr.io/deep-learning-254808/$tag" \
-        --platform=managed \
-        --allow-unauthenticated \
-        --region=us-east1 \
-        --concurrency=1 \
-        --memory=2Gi
-
-    # Delete images on GCR to save money?
-   [ "$question" != "${question#[Yy]}" ] && gcloud container images delete --force-delete-tags "gcr.io/deep-learning-254808/$tag"
-   
-done
+[ "$question" != "${question#[Yy]}" ] && gcloud container images delete --force-delete-tags "gcr.io/$project_id/$tag"
 
 echo -e "\nDone!\nURLs of Cloud Run services:\n\n$(gcloud beta run services list --platform=managed |grep -Po 'https.?://\S+')"
